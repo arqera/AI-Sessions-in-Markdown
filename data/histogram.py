cc9a69138e7408ab30420a4f51b69b9e451ca793
arqera <arqera@protonmail.com>
2023-03-18 12:09:05 -0700

    fixed code again

--- data/histogram.py
+++ data/histogram.py
@@ -1,19 +1,17 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-digit_freqs = {str(i): 0 for i in range(10)}
+digit_freqs = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
 
-with open("Humanity_and_the_Environmen.txt") as f:
+with open('Humanity_and_the_Environmen.txt', 'r') as f:
     for line in f:
-        if int(line[:4]) > 1999:
-            for char in line:
-                if char.isdigit():
-                    digit_freqs[char] += 1
+        try:
+            if int(line[:4]) > 1999:
+                for digit in line:
+                    if digit.isdigit():
+                        digit_freqs[int(digit)] += 1
+        except ValueError:
+            continue
 
-fig, ax = plt.subplots()
-ax.bar(digit_freqs.keys(), digit_freqs.values())
-ax.set_xlabel('Digit')
-ax.set_ylabel('Frequency')
-ax.set_title('Frequency of Digits in Humanity and the Environment Dataset')
+plt.bar(digit_freqs.keys(), digit_freqs.values())
 plt.show()
 
