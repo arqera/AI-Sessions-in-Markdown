e03b41d032b625262a88f8fba5243a3bb7e532ed
arqera <arqera@protonmail.com>
2023-03-18 18:38:28 -0700

    anomalous add

--- data/histogram.py
+++ data/histogram.py
@@ -5,7 +5,7 @@ digit_freqs = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
 with open('Humanity_and_the_Environmen.txt', 'r') as f:
     for line in f:
         try:
-            if int(line[:4]) > 1999:
+            if int(line[:4]) > 1923:
                 for digit in line:
                     if digit.isdigit():
                         digit_freqs[int(digit)] += 1
